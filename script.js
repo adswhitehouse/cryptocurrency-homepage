d41f8180a2daac7eaa2bcddc877938248f22c06a
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-03-04 21:50:01 +0000

    crypto prices are synced

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 // Variables
 let menu = document.querySelector(".jsMenu");
-let header = document.querySelector(".jsHeader");
+let nav = document.querySelector(".jsNav");
+let bitcoinPrice = document.querySelector(".jsBitcoinPrice");
+let ethereumPrice = document.querySelector(".jsEthereumPrice");
+let dogecoinPrice = document.querySelector(".jsDogecoinPrice");
 
 // Animate header
 menu.addEventListener("click", () => {
-  header.classList.toggle("header-animation");
+  nav.classList.toggle("nav-animation");
   menu.classList.toggle("rotate");
+});
+
+let settings = {
+  async: true,
+  scrossDomain: true,
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=gbp",
+  method: "GET",
+  headers: {},
+};
+
+$.ajax(settings).done(function (response) {
+  bitcoinPrice.textContent = `£${response.bitcoin.gbp}`;
+  ethereumPrice.textContent = `£${response.ethereum.gbp}`;
+  dogecoinPrice.textContent = `£${response.dogecoin.gbp}`;
 });
